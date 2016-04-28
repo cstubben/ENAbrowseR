9fba99ff8ec144cf5553eb26d66d02682b0d2dfa
Chris Stubben <chris.stubben@gmail.com>
2016-04-28 11:56:03 -0600

    add sum_bases

--- R/sum_bases.R
+++ R/sum_bases.R
@@ -1,5 +1,6 @@
 sum_bases <-function(x, round=0)
 {
+x <- sum(as.numeric(x), na.rm=TRUE)
 ifelse( x< 10^3, paste(x, "bp"),
  ifelse(x< 10^6, paste(round( x/ 10^3, round) , "kbp"),
    ifelse(x< 10^9, paste(round( x/ 10^6, round) , "Mbp"), 
